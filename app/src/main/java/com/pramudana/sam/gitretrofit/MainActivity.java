36ef07b630e28768737fc6d4a39b9bd12c68aa97
Sampramudana <32828518+Sampramudana@users.noreply.github.com>
2018-08-24 14:08:15 +0700

    Finish code

--- app/src/main/java/com/pramudana/sam/gitretrofit/MainActivity.java
+++ app/src/main/java/com/pramudana/sam/gitretrofit/MainActivity.java
@@ -2,12 +2,58 @@ package com.pramudana.sam.gitretrofit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.pramudana.sam.gitretrofit.adapter.CustomAdapter;
+import com.pramudana.sam.gitretrofit.api.ApiService;
+import com.pramudana.sam.gitretrofit.api.InstanceRetrofit;
+import com.pramudana.sam.gitretrofit.response.DataItem;
+import com.pramudana.sam.gitretrofit.response.ResponseData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    //TODO Create RecyclerView variable class
+    RecyclerView recyclerView;
+    String name, address;
+    List<DataItem> dataItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO Inlitialize Widget to Variable
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        getData();
+    }
+
+    private void getData() {
+        ApiService api = InstanceRetrofit.getInstance();
+        Call<ResponseData> call = api.response_read_data();
+        call.enqueue(new Callback<ResponseData>() {
+            @Override
+            public void onResponse(Call<ResponseData> call, Response<ResponseData> response) {
+                Boolean status = response.body().isSuccess();
+                if (status){
+                    dataItems = response.body().getData();
+                    CustomAdapter adapter = new CustomAdapter(MainActivity.this, dataItems);
+                    recyclerView.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseData> call, Throwable t) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/pramudana/sam/gitretrofit/adapter/CustomAdapter.java
@@ -0,0 +1,69 @@
+package com.pramudana.sam.gitretrofit.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.pramudana.sam.gitretrofit.R;
+import com.pramudana.sam.gitretrofit.response.DataItem;
+
+import java.util.List;
+
+/**
+ * Created by sampramudana on 8/24/18.
+ */
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
+
+    Context context;
+    List<DataItem> data;
+    public static final String WebUrl = "http://192.168.10.52./GitRetrofit/";
+
+    public CustomAdapter(Context context, List<DataItem> dataItems) {
+        this.context = context;
+        this.data = dataItems;
+    }
+
+    @Override
+    public CustomAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_list, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(CustomAdapter.MyViewHolder holder, int position) {
+
+        final String url_image = WebUrl+"upload/"+data.get(position).getImage();
+        final String strId = data.get(position).getId().toString();
+        holder.name.setText(data.get(position).getName());
+        holder.address.setText(data.get(position).getAddress());
+        Glide.with(context)
+                .load(url_image)
+                .placeholder(R.mipmap.ic_launcher)
+                .into(holder.img);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView name, address;
+        ImageView img;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            name = itemView.findViewById(R.id.txtName);
+            address = itemView.findViewById(R.id.txtAddress);
+
+        }
+    }
+}
